d2ffe2b3a909c3b2d953d7a1e05b4e5c037ccbfd
Diren Tikil <diren@tikil.de>
2023-02-14 15:59:28 +0100

    confriminput

--- pages/flightCard.js
+++ pages/flightCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useAtom } from "jotai";
 import globalTrips from "@/public/data";
 
-export default function FlightCard({ trip }) {
+export default function FlightCard() {
   const [trip, setTrip] = useAtom(globalTrips);
 
   return <FlightCard trip={trip} />;
