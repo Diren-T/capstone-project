0e292e66f42cc5e67b3e695daa36ac90a8bdb777
Diren Tikil <diren@tikil.de>
2023-02-14 15:40:26 +0100

    confirminput

--- pages/flightCard.js
+++ pages/flightCard.js
@@ -5,13 +5,6 @@ import globalTrips from "@/public/data";
 export default function FlightCard({ trip }) {
   const [trips, setTrips] = useAtom(globalTrips);
 
-  //   function handelDeleteItem(id) {
-  //     setTrips(
-  //       trips.filter((trip) => {
-  //         return trip.id !== id;
-  //       })
-  //     );
-  //   }
   return (
     <article>
       <section>
@@ -19,7 +12,6 @@ export default function FlightCard({ trip }) {
         <p>Destination: {trip.to}</p>
         <p>Passengers: {trip.passengerCount}</p>
         <p>co2: {parseInt(trip.co2e)} kg</p>
-        {/* <button onClick={() => handelDeleteItem(trip.id)}>Delete </button> */}
       </section>
     </article>
   );
