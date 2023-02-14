c0de64976b4d0edc78ef4ff8bc4a75bac3c7ab70
Diren Tikil <diren@tikil.de>
2023-02-14 16:05:31 +0100

    confirminput

--- components/Card/Index.js
+++ components/Card/Index.js
@@ -1,12 +1,10 @@
 export default function FlightCard({ trip }) {
   return (
-    <article>
-      <section>
-        <p>Departure: {trip.from}</p>
-        <p>Destination: {trip.to}</p>
-        <p>Passengers: {trip.passengerCount}</p>
-        <p>co2: {parseInt(trip.co2e)} kg</p>
-      </section>
-    </article>
+    <section>
+      <p>Departure: {trip.from}</p>
+      <p>Destination: {trip.to}</p>
+      <p>Passengers: {trip.passengerCount}</p>
+      <p>co2: {parseInt(trip.co2e)} kg</p>
+    </section>
   );
 }
--- pages/flightCard.js
+++ pages/flightCard.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useAtom } from "jotai";
 import globalTrips from "@/public/data";
+import FlightCard from "@/components/Card/Index";
 
-export default function FlightCard() {
+export default function FlightCardPage() {
   const [trip, setTrip] = useAtom(globalTrips);
 
   return <FlightCard trip={trip} />;
